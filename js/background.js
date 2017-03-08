0e157fbe62da56a4667d45f45c824f02bb6bf52f
Arighyni <arighyni@wanadev.fr>
2017-03-08 11:30:25 +0100

    chart soon

--- js/background.js
+++ js/background.js
@@ -29,36 +29,79 @@ if (typeof localStorage.targetCurrency === "undefined") {
 
 function updateTicker() {
 
-jQuery.getJSON(
-    "https://api.coinbase.com/v2/prices/"+ localStorage.targetCurrency +"-"+ localStorage.sourceCurrency +"/spot",
-    function (data, txtStatus, xhr) {
-        priceString = data.data.amount.toString();
-        price = data.data.amount;
-        chrome.browserAction.setBadgeText({text: priceString});
-        if(price > localStorage.alertValue && localStorage.alertValue > 0){
+    jQuery.getJSON(
+        "https://api.coinbase.com/v2/prices/"+ localStorage.targetCurrency +"-"+ localStorage.sourceCurrency +"/spot",
+        function (data, txtStatus, xhr) {
+            priceString = data.data.amount.toString();
+            price = data.data.amount;
+            chrome.browserAction.setBadgeText({text: priceString});
+            if(price > localStorage.alertValue && localStorage.alertValue > 0){
 
-            chrome.notifications.create("price", {
-                type: "basic",
-                title: localStorage.targetCurrency + " price is over " + localStorage.alertValue,
-                message: localStorage.targetCurrency + " rate price is" + priceString,
-                iconUrl: "img/icon.png"
-                }, function () {
-            });
-        }
+                chrome.notifications.create("price", {
+                    type: "basic",
+                    title: localStorage.targetCurrency + " price is over " + localStorage.alertValue,
+                    message: localStorage.targetCurrency + " rate price is" + priceString,
+                    iconUrl: "img/icon.png"
+                    }, function () {
+                });
+            }
 
-        if(price < localStorage.panicValue && localStorage.panicValue > 0){
+            if(price < localStorage.panicValue && localStorage.panicValue > 0){
 
-            chrome.notifications.create("price", {
-                type: "basic",
-                title: localStorage.targetCurrency + " price is lower " + localStorage.panicValue,
-                message: localStorage.targetCurrency + " rate price is" + priceString,
-                iconUrl: "img/icon.png"
-                }, function () {
-            });
-        }
+                chrome.notifications.create("price", {
+                    type: "basic",
+                    title: localStorage.targetCurrency + " price is lower " + localStorage.panicValue,
+                    message: localStorage.targetCurrency + " rate price is" + priceString,
+                    iconUrl: "img/icon.png"
+                    }, function () {
+                });
+            }
+
+    });
+
+    var dateObj = new Date();
+    var month = dateObj.getUTCMonth() + 1; //months from 1-12
+    var day = dateObj.getUTCDate();
+    var year = dateObj.getUTCFullYear();
+    var monthString, dayString;
+
+    //alert(year +""+ month+"" + day);
+
+    if(month < 10){
+      monthString = "0" + month;
+    } else{
+      monthString = month.toString();
+    }
+
+    if(day < 10){
+      dayString = "0" + (day - 1);
+    } else{
+      dayString = day.toString();
+    }
+
+    var dateString = year + "-" + monthString + "-" + dayString;
+    //alert(dateString);
+
+    jQuery.getJSON(
+        "https://min-api.cryptocompare.com/data/histominute?fsym=ETH&tsym=EUR&limit=60&aggregate=3&e=CCCAGG",
+        //"https://api.coinbase.com/v2/prices/"+ localStorage.targetCurrency +"-"+ localStorage.sourceCurrency +"/spot?date=" + dateString,
+        function (data, txtStatus, xhr) {
+
+          var sum = 0;
+
+          for(var i = 0; i < data.Data.length; i++){
+            sum += data.Data[i].low;
+          }
+
+          //alert(sum / 60);
+
+            /*priceString = data.data.amount.toString();
+            price = data.data.amount;*/
+            //alert(data.Data[0].low);
 
     });
 }
+
 
 window.setInterval(updateTicker, localStorage.delay);
 
