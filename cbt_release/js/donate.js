131545227fee2a78d486802f6bb03321881ed1df
Bob Razowsky <aymeric.righyni@gmail.com>
2017-08-29 23:45:45 +0200

    added latest release

---
+++ cbt_release/js/donate.js
@@ -0,0 +1,1 @@
+function translate(){document.getElementById("strDonations").innerHTML=chrome.i18n.getMessage("strDonations"),document.getElementById("strDonationsTxt").innerHTML=chrome.i18n.getMessage("strDonationsTxt")}translate();
