f5575fa65731c065eefc4a967a226f5d656a95eb
Bob Razowsky <aymeric.righyni@gmail.com>
2017-09-11 13:24:22 +0200

    pretty grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,83 +1,91 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
+	grunt.initConfig({
 
-	pkg: grunt.file.readJSON('package.json'),
+		pkg: grunt.file.readJSON('package.json'),
 
-	watch: {
-	  scripts: {
-		files: ['js/*.js', 'less/*.less', '*.html'],
-		tasks: ['build'],
-		options: {
-		  spawn: false
-		}
-	  }
-	},
-
-	copy: {
-	  main: {
-		files: [
-		  {expand: true, src: ['*.html'], dest: 'cbt_release/'},
-		  {expand: true, src: ['img/*'], dest: 'cbt_release/'},
-		  {expand: true, src: ['css/*.css'], dest: 'cbt_release/'},
-		  {expand: true, src: ['_locales/**'], dest: 'cbt_release/'},
-		  {expand: true, src: ['sounds/*'], dest: 'cbt_release/'},
-		  {expand: true, src: ['vendors/*'], dest: 'cbt_release/'},
-		  {expand: true, src: ['manifest.json'], dest: 'cbt_release/'}
-		]
-	  }
-	},
-
-	less: {
-	  production: {
-		options: {
-		  path: ['css']
+		watch: {
+			scripts: {
+				files: ['js/*.js', 'less/*.less', '*.html'],
+				tasks: ['build'],
+				options: {
+					spawn: false
+				}
+			}
 		},
-		files: {
-		  'cbt_release/css/popup.css':'less/popup.less',
-		  'cbt_release/css/donate.css':'less/donate.less',
-		  'cbt_release/css/options.css':'less/options.less'
-		}
-	  }
-	},
 
-	uglify: {
-	  main: {
-		files: [
-		  {
-			  expand: true,
-			  src: 'js/*.js',
-			  dest: 'cbt_release/'
-		  },
-		  {
-			  expand: true,
-			  src: 'vendors/*.js',
-			  dest: 'cbt_release/'
-		  }
-		]
-	  }
-	},
-
-	compress: {
-	  main: {
-		options: {
-		  archive: 'oldPackages/<%= pkg.short %><%= pkg.version %>.zip'
+		copy: {
+			main: {
+				files: [
+					{expand: true, src: ['*.html'], dest: 'cbt_release/'},
+					{expand: true, src: ['img/*'], dest: 'cbt_release/'},
+					{expand: true, src: ['css/*.css'], dest: 'cbt_release/'},
+					{expand: true, src: ['_locales/**'], dest: 'cbt_release/'},
+					{expand: true, src: ['sounds/*'], dest: 'cbt_release/'},
+					{expand: true, src: ['vendors/*'], dest: 'cbt_release/'},
+					{expand: true, src: ['manifest.json'], dest: 'cbt_release/'}
+				]
+			}
 		},
-		files: [
-		  {src: ['cbt_release/**'], dest: '/'}
-		]
-	  }
-	}
-  });
-  
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-compress');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.registerTask('default', ['less']);
-  grunt.registerTask('build', ['copy', 'uglify', 'less']);
-  grunt.registerTask('pack', ['compress']);
-  grunt.registerTask('release', ['copy', 'uglify', 'less', 'compress']);
+		less: {
+			production: {
+				options: {
+					path: ['css']
+				},
+				files: {
+					'cbt_release/css/popup.css':'less/popup.less',
+					'cbt_release/css/donate.css':'less/donate.less',
+					'cbt_release/css/options.css':'less/options.less'
+				}
+			}
+		},
+
+		uglify: {
+			main: {
+				files: [{
+					expand: true,
+					src: 'js/*.js',
+					dest: 'cbt_release/'
+				},
+				{
+					expand: true,
+					src: 'vendors/*.js',
+					dest: 'cbt_release/'
+				}]
+			}
+		},
+
+		compress: {
+			main: {
+				options: {
+					archive: 'oldPackages/<%= pkg.short %><%= pkg.version %>.zip'
+				},
+				files: [
+					{src: ['cbt_release/**'], dest: '/'}
+				]
+			},
+			firefox: {
+				options: {
+					archive: 'oldPackages/<%= pkg.short %><%= pkg.version %>_firefox.zip'
+				},
+				files: [{
+					src: ['**/*'],
+					cwd: 'cbt_release/',
+					expand: true
+				}]
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+
+	grunt.registerTask('default', ['less']);
+	grunt.registerTask('build', ['copy', 'uglify', 'less']);
+	grunt.registerTask('pack', ['compress']);
+	grunt.registerTask('release', ['copy', 'uglify', 'less', 'compress']);
 };
